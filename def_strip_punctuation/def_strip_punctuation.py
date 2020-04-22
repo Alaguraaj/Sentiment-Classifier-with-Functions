023c6bef6c4aa21667d78a39bf34bb14d41c1343
Alaguraaj <53381693+Alaguraaj@users.noreply.github.com>
2020-04-22 17:55:04 +0200

    Create def_strip_punctuation.py

---
+++ def_strip_punctuation/def_strip_punctuation.py
@@ -0,0 +1,13 @@
+punctuation_chars = ["'", '"', ",", ".", "!", ":", ";", '#', '@']
+
+#with open("project_twitter_data.csv ","r") as twitter :
+#    print (twitter.read())
+    
+def strip_punctuation(words):
+    for i in punctuation_chars:
+        words=words.replace(i,'')
+    print(words)
+    return words
+    
+check=strip_punctuation('Strip, ssss!!!')
+print(check)
